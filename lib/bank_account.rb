dde993642291fa1d1ce58cc579bc835e2822897b
Winston Chen <winstonchen.wm@gmail.com>
2020-11-23 01:32:06 -0500

    commit tree 784a0bd9ea9c93b6ef16090609824677a5d4fb1a

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class BankAccount
 
+    attr_accessor :balance, :status 
+    attr_reader :name 
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def deposit(deposit_amount) 
+        @balance += deposit_amount 
+    end
+
+    def display_balance
+        p "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        if @status == "open" && @balance > 0
+          true
+        else
+          false
+        end
+    end
+
+    def close_account
+        @status = "closed"
+    end
+
+
 end
+
+winston_account = BankAccount.new("winston")
+
+# binding.pry 
+
